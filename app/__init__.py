d329a5f5c134cb5d5a007f5a4ccf68887177a46c
mcarina <marciacarina931@gmail.com>
2024-10-17 15:31:07 -0400

    at

--- app/__init__.py
+++ app/__init__.py
@@ -16,14 +16,11 @@ def create_app():
 
     app.config['SQLALCHEMY_DATABASE_URI'] = uri_modificada
     app.config["JWT_SECRET_KEY"] = "senha"
-    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=1)
-    app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(hours=5)
 
     api.init_app(app, authorizations=authorizations)
     db.init_app(app)
     jwt.init_app(app)
 
-    # CORS(app, resources={r"/api/*": {"origins": ["http://127.0.0.1:3000", "http://172.16.17.51:3000"]}})    
     CORS(app, resources={r"/api/*": {"origins": "*"}})
 
     api.add_namespace(ns)
